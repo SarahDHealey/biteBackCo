266e1241e20e1e4ca27dcf73484fd6ec27c2bcc5
Sarah Healey <s.deane.healey@gmail.com>
2017-04-09 08:21:07 -0600

    practice grid layout

--- index.ios.js
+++ index.ios.js
@@ -9,39 +9,65 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView,
+  Dimensions,
 } from 'react-native';
 
 export default class biteBackCo extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {myText: "Hello World"};
-  }
-  
-  changeMyText(value) {
-    this.setState({ myText: value });
-  }
-
   render() {
     return (
-      <View>
-        <Text>
-          {this.state.myText}
-        </Text>
-        <Text onPress={ () => {this.changeMyText('Hello, Saturnus')}}>
-          "change text to hello Saturnus"
-        </Text>
-        <Text onPress={ () => {this.changeMyText('Hello, Saturnus')}}>
-          "change text to hello Saturnus"
-        </Text>
-      </View>
-    );
+      <ScrollView style={styles.scrollContainer}>
+        <View style={styles.container}>
+          <View style={styles.box}>
+            <Text>Box1</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Box2</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Box3</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Box4</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Box5</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Box6</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Box7</Text>
+          </View>
+          <View style={styles.box}>
+            <Text>Box8</Text>
+          </View>
+        </View>
+      </ScrollView>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-
+  scrollContainer: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 2,
+  },
+  box: {
+    margin: 2,
+    width: Dimensions.get('window').width / 2 -6,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f1c40f',
+    height: 200,
+  },
 });
 
 AppRegistry.registerComponent('biteBackCo', () => biteBackCo);
